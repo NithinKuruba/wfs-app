5b67eb206239f613cc8d6ae7080f0f9b78e88a73
NithinKuruba <nithinshekar.kuruba@gmail.com>
2021-03-28 16:55:02 -0700

    add new functionalities
    1. mysql driver connectivity from api
    2. docker and docker compose to build and deploy api and ui

--- wfs-api/server.ts
+++ wfs-api/server.ts
@@ -1,15 +1,37 @@
 import express from 'express'
 import path from 'path'
+import mysql from 'mysql2/promise'
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'wfs',
+  password: 'wfs',
+  database: 'analytics',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+})
 
 const app = express()
 const port = 3080
 
 app.use(express.json())
-app.use(express.static(path.join(__dirname, '../wfs-ui/build')))
+app.use(express.static(path.join(__dirname, '../../wfs-ui/build')))
 
-app.get('/hello', (req, res) => {
-  console.log('Hello World!!!!')
-  res.json('Hello World!!!!')
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../wfs-ui/build/index.html'))
+})
+
+app.post('/track', (req, res) => {
+  const id = req.body.id
+  const name = req.body.name
+  insertRecord(id, name)
+    .then(() => {
+      res.send('OK')
+    })
+    .catch((err) => {
+      throw err
+    })
 })
 
 // start the express server
@@ -17,3 +39,17 @@ app.listen(port, () => {
   // tslint:disable-next-line:no-console
   console.log(`server started at http://localhost:${port}`)
 })
+
+async function insertRecord(id: any, name: any): Promise<void> {
+  const sql = 'INSERT INTO auditlog (id, name) VALUES(?, ?)'
+  const connection = await pool.getConnection()
+  try {
+    await connection.query(sql, [id, name])
+    await connection.commit()
+  } catch (err) {
+    await connection.rollback()
+    throw err
+  } finally {
+    connection.release()
+  }
+}
