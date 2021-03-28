6e553b3041df557e0acaae2bf7c1dbe6f768bfa9
NithinKuruba <nithinshekar.kuruba@gmail.com>
2021-03-27 17:43:52 -0700

    Implemented feature #2
    1. Throwing an error message if lat/lng entered does not belong to BC
    2. Added a helper link to find lat/lng on a map

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,3 +2,5 @@ export const RESPONSE_FORMAT = 'application/json'
 export const API_BASE_URL =
   'https://openmaps.gov.bc.ca/geo/pub/ows?service=WFS&version=1.0.0'
 export const REQUEST_TYPE_GET_FEATURE = '&request=GetFeature'
+export const CHSA_IMAP_LINK =
+  'https://maps.gov.bc.ca/ess/hm/imap4m/?catalogLayers=7705,7706'
